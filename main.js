800d1a3bbf0b6141c158dc80c48ca1982f355044
Marco von Hagen <m4rco.mail@web.de>
2015-06-18 12:47:32 +0200

    added line

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 /*global $, define, brackets, Mustache */
 
 // Changes made when in branch `master`.
-// added line in develop
+// added line in develop...
 
 //DESCRIPTION:Add an EventListener on Document.afterSave().
 
